fab8531d39ef0d7e6797d9a4b2f90b12e6956a47
Angelo Raso <angeloraso6@gmail.com>
2019-12-28 20:01:40 -0300

    Create html per section and includes them

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const DATE_OF_BIRTH = new Date(1993, 6, 5); // 5 de Junio de 1993
+// Incluye htmls de las diferentes secciones
+function includeHtml( activate ) {
+    const includes = document.getElementsByTagName('include');
+    [].forEach.call(includes, i => {
+        let filePath = i.getAttribute('src');
+        fetch(filePath).then(file => {
+            file.text().then(content => {
+                i.insertAdjacentHTML('afterend', content);
+                i.remove();
+                activate();
+            });
+        });
+    });
+}
 
-var age = document.getElementById('my-age');
+includeHtml( activate );
 
-age.innerText = calculateAge(DATE_OF_BIRTH);
-
-function calculateAge( date_of_birth ) {
-    var diff_ms = Date.now() - date_of_birth.getTime();
-    var age_dt = new Date(diff_ms);
-    return Math.abs(age_dt.getUTCFullYear() - 1970);
+function activate() {
+    const DATE_OF_BIRTH = new Date(1993, 6, 5); // 5 de Junio de 1993
+    
+    var age = document.getElementById('my-age');
+    
+    age.innerText = calculateAge(DATE_OF_BIRTH);
+    
+    function calculateAge(date_of_birth) {
+        var diff_ms = Date.now() - date_of_birth.getTime();
+        var age_dt = new Date(diff_ms);
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
 }
