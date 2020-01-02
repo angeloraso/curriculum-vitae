9e3805d90ed31b228719f00da5d5b021dbdcd9c4
Angelo Raso <angeloraso6@gmail.com>
2020-01-01 21:40:49 -0300

    Add modal popup on skills

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function includeHtml() {
 includeHtml();
 
 function activate() {
+    // Setea la edad segun la fecha de cumpleaños
     var DATE_OF_BIRTH = new Date(1993, 6, 5); // 5 de Junio de 1993
     
     var age = document.getElementById('my-age');
@@ -33,12 +34,24 @@ function activate() {
         return Math.abs(age_dt.getUTCFullYear() - 1970);
     }
 
+    // Seta el año actual para los derechos reservados en el footer
     var current_year = document.getElementById('current-year');
     current_year.innerText = new Date().getUTCFullYear();
 
-    [].forEach.call(document.getElementsByClassName('up-arrow'), item => {
-        item.addEventListener('click', () => {
+    // Agrega click listener a las flechas
+    document.querySelectorAll('.up-arrow').forEach(item => {
+        item.addEventListener('click', event => {
             document.location.href = "#";
+        })
+    })
+    // Agrega click listener a los skills
+    var iframe = document.getElementById('popup-iframe');
+    var close = document.getElementById('popup-close');
+    document.querySelectorAll('.skill').forEach(item => {
+        item.addEventListener('click', event => {
+            document.location.href = "#popup-article";
+            iframe.src = item.id;
+            close.href = '#' + item.id;
         })
     })
 }
